3cf48fd40064d33f98a64d78f93c003e3f81d962
rodvama <rodvama@gmail.com>
2018-05-10 14:18:37 +0000

    cortos cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-    	<h1>Lista de opciones</h1>
-    </div>
-    <div class="row"> 	
-			<a class="waves-effect waves-light btn-large" href="{{ url('maestros') }}"><i \class="material-icons right">cloud</i>Maestros</a>
-    </div>
-    <div class="row"> 	
-			<a class="waves-effect waves-light btn-large" href="{{ url('salones') }}"><i \class="material-icons right">cloud</i>Salones</a>
+    		<h1 style="font-size: 600%;">Bienvenido</h1>
+    	</div>
+    	<div class="row justify-content-center">
+    	<h2>{{Auth::user()->name}}</h2>
     </div>
 </div>
 @endsection
